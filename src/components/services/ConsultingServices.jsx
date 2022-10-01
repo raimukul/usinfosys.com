4e101f97d27f095fc308a13d79c0755d848d535f
Mukul Rai <47410631+raimukul@users.noreply.github.com>
2022-10-02 00:07:04 +0530

    Update ConsultingServices.jsx

--- src/components/services/ConsultingServices.jsx
+++ src/components/services/ConsultingServices.jsx
@@ -6,7 +6,30 @@ export default function ConsultingServices() {
   return (
     <div>
       <Container>
-        <Card className="p-2">
+        <div className="pt-2">
+          <Card className="p-4">
+          <Typography variant="h5">Consulting Services</Typography>
+          <Divider className="bg-dark w-25" variant="string" />
+            <Typography className="pt-2" variant="body1">
+              AIS Consulting Services help clients solve their Software Quality
+              Assurance challenges and achieve the quality goal. Our Consulting
+              offerings are based on years of progressive experience assisting
+              customers to meet their quality needs. Software Testing Services
+              of AIS consulting services offering is delivered by a team of
+              consultants including architects and specialists with in-depth
+              industry, business, technology, and quality process. These
+              seasoned consultants have empowered a range of companies to
+              embrace industry leading technologies that align with their unique
+              quality goals. We help clients to identify and implement best
+              practice solutions to address the unique business challenges.
+              These efficiently delivered solutions help you transform your
+              business with targeted investments toward growth, optimized
+              efficiency and predictable total cost of ownership (TCO), and
+              well-managed risk and compliance around business operations.
+            </Typography>
+          </Card>
+        </div>
+        <Card className="p-4">
           <Typography variant="h5">Our IT Consulting Services</Typography>
           <Divider className="bg-dark w-25" variant="string" />
           <Grid container spacing={3} className="pt-5">
@@ -15,8 +38,11 @@ export default function ConsultingServices() {
                 <img
                   className="w-50 rounded mx-auto d-block"
                   src="./services/technology.png"
-                alt="1q"
+                  alt="1q"
                 ></img>
+                <Typography className="text-center pt-2 pb-2">
+                  Architecture and Technology
+                </Typography>
               </Card>
             </Grid>
             <Grid item xs={12} md={6} lg={4}>
@@ -24,8 +50,11 @@ export default function ConsultingServices() {
                 <img
                   className="w-50 rounded mx-auto d-block"
                   src="./services/risks.png"
-                alt="ef"
+                  alt="ef"
                 ></img>
+                <Typography className="text-center pt-2 pb-2">
+                  Information Risk Management
+                </Typography>
               </Card>
             </Grid>
             <Grid item xs={12} md={6} lg={4}>
@@ -33,8 +62,11 @@ export default function ConsultingServices() {
                 <img
                   className="w-50 rounded mx-auto d-block"
                   src="./services/infrastructure.png"
-                alt="rgtg"
+                  alt="rgtg"
                 ></img>
+                <Typography className="text-center pt-2 pb-2">
+                  Infrastructure Services
+                </Typography>
               </Card>
             </Grid>
             <Grid item xs={12} md={6} lg={4}>
@@ -42,8 +74,11 @@ export default function ConsultingServices() {
                 <img
                   className="w-50 rounded mx-auto d-block"
                   src="./services/execution.png"
-                alt="gade"
+                  alt="gade"
                 ></img>
+                <Typography className="text-center pt-2 pb-2">
+                  IT Process and Service Management
+                </Typography>
               </Card>
             </Grid>
             <Grid item xs={12} md={6} lg={4}>
@@ -51,8 +86,11 @@ export default function ConsultingServices() {
                 <img
                   className="w-50 rounded mx-auto d-block"
                   src="./services/quality-test.png"
-                alt="asrgtaewg"
+                  alt="asrgtaewg"
                 ></img>
+                <Typography className="text-center pt-2 pb-2">
+                  IT Strategy and Governance
+                </Typography>
               </Card>
             </Grid>
           </Grid>
