dab1f498fa0ce14d10d552b938f469b95fde6ef6
Mukul Rai <47410631+raimukul@users.noreply.github.com>
2022-10-02 23:22:33 +0530

    Update About.jsx

--- src/components/services/DatabaseServices/About.jsx
+++ src/components/services/DatabaseServices/About.jsx
@@ -6,20 +6,20 @@ export default function About() {
   return (
     <div>
       <Container>
-        <Card className="p-3 pt-5">
-      <div className="pt-2 pb-2">
-      <Typography variant="body1">
-        From day-to-day operations to strategic decision making, your databases
-        are the engines that drive your business. Database Services SQL Server
-        technicians have the experience and skill sets you need to ensure that
-        your database environments have high performance, security, and
-        availability. AIS offer a comprehensive set of well-defined enterprise
-        database services. These services are focused on Desktop databases like
-        MySQL, Access to enterprise database solutions like Oracle, SQL Server,
-        DB2 and FileMaker Pro.
-      </Typography>
-      </div>
-      </Card>
+        <Card className="p-3">
+          <div className="pt-2 pb-2">
+            <Typography variant="body1">
+              From day-to-day operations to strategic decision making, your
+              databases are the engines that drive your business. Database
+              Services SQL Server technicians have the experience and skill sets
+              you need to ensure that your database environments have high
+              performance, security, and availability. AIS offer a comprehensive
+              set of well-defined enterprise database services. These services
+              are focused on Desktop databases like MySQL, Access to enterprise
+              database solutions like Oracle, SQL Server, DB2 and FileMaker Pro.
+            </Typography>
+          </div>
+        </Card>
       </Container>
     </div>
   );
