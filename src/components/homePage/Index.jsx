e7a325b07569d3c4b53bcea2580de9d4f2a32ef3
Mukul Rai <47410631+raimukul@users.noreply.github.com>
2022-10-08 03:55:08 +0530

    update

--- src/components/homePage/Index.jsx
+++ src/components/homePage/Index.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Carousel from './Carousel'
 import Testimonial from './Testimonial'
+import Test from './Test'
 
 export default function Index() {
   return (
     <div>
       <Carousel/>
+      {/* <Test/> */}
       <Testimonial/>
     </div>
   )
